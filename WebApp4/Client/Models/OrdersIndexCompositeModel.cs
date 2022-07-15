df40f131581b2a3cee072e15757aa5cfcf60c005
Mark Phillips <mark_phillips21@hotmail.com>
2022-07-15 11:41:03 +1000

    Removed unused projects

--- WebApp4/Client/Models/OrdersIndexCompositeModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApp4.Client.Models
-{
-    public class OrdersIndexCompositeModel
-    {
-        public Sales.API.Model.OrdersIndexViewModel? Sales { get; set; }
-        public Catalog.API.Model.OrdersIndexViewModel? Catalog { get; set; }
-    }
-}
